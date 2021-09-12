906655b0f0ce9674902ae1c160ef87a17c19be18
Keiichi Fukaya <fukayak99@gmail.com>
2021-09-12 14:56:06 +0900

    Improve tests for set_const()

--- tests/testthat/test-occumb.R
+++ tests/testthat/test-occumb.R
@@ -4,11 +4,17 @@ test_that("Replacement of missing y to NA works", {
     y[, 1, 1] <- 0
     result    <- set_const(occumbData(y = y))
     expect_identical(result$y[, 1, 1], as.numeric(rep(NA, 2)))
+    expect_identical(result$y[, 2, 1], c(3, 4))
+    expect_identical(result$y[, 1, 2], c(5, 6))
+    expect_identical(result$y[, 2, 2], c(7, 8))
 })
 test_that("Replacement of sequence depth 0 to 1 works", {
     y         <- array(1:8, dim = rep(2, 3))
     y[, 1, 1] <- 0
     result    <- set_const(occumbData(y = y))
     expect_equal(result$N[1, 1], 1)
+    expect_equal(result$N[2, 1], sum(3:4))
+    expect_equal(result$N[1, 2], sum(5:6))
+    expect_equal(result$N[2, 2], sum(7:8))
 })
 
