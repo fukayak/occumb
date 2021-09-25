72c6798fb7eb2272aadcc1ae690788b5eb0bd05b
Keiichi Fukaya <fukayak99@gmail.com>
2021-09-25 23:31:00 +0900

    Add tests for psi output in set_modargs()

--- tests/testthat/test-occumb.R
+++ tests/testthat/test-occumb.R
@@ -19,82 +19,97 @@ test_that("Replacement of sequence depth 0 to 1 works", {
 })
 
 ### Tests for set_modargs() ----------------------------------------------------
-test_that("Setup for a null model works", {
-    result <- set_modargs(~ 1, ~ 1, ~ 1,
-                          occumbData(y = array(0, dim = rep(2, 3))))
-    expect_equal(result$M, 3)
-    expect_equal(result$code, "111000")
-    expect_equal(result$cov_phi, 1)
-    expect_equal(result$cov_theta, 1)
-    expect_equal(result$cov_psi, 1)
-    expect_equal(result$m_phi, 1)
-    expect_equal(result$m_theta, 2)
-    expect_equal(result$m_psi, 3)
-})
-test_that("Setup for varying-psi model works", {
-    cov1 <- rnorm(2); cov2 <- factor(1:2)
+test_that("Temp: psi correct", {
+    cov1 <- cov2 <- rnorm(2); cov3 <- factor(1:2)
     data <- occumbData(y = array(0, dim = rep(2, 3)),
-                       site_cov = list(cov1 = cov1,
-                                       cov2 = cov2))
-
-    # Continuous effect
-    result <- set_modargs(~ 1, ~ 1, ~ cov1, data)
-    mm     <- model.matrix(as.formula(~ cov1), data@site_cov)
-    expect_equal(result$M, 4)
-    expect_equal(result$code, "112000")
-    expect_equal(result$cov_phi, 1)
-    expect_equal(result$cov_theta, 1)
-    expect_equal(result$cov_psi, mm)
-    expect_equal(result$m_phi, 1)
-    expect_equal(result$m_theta, 2)
-    expect_equal(result$m_psi, 3:4)
-
-    # Discrete effect
-    result <- set_modargs(~ 1, ~ 1, ~ cov2, data)
-    mm     <- model.matrix(as.formula(~ cov2), data@site_cov)
-    expect_equal(result$M, 4)
-    expect_equal(result$code, "112000")
-    expect_equal(result$cov_phi, 1)
-    expect_equal(result$cov_theta, 1)
-    expect_equal(result$cov_psi, mm)
-    expect_equal(result$m_phi, 1)
-    expect_equal(result$m_theta, 2)
-    expect_equal(result$m_psi, 3:4)
-
-    # Additive effects
-    result <- set_modargs(~ 1, ~ 1, ~ cov1 + cov2, data)
-    mm     <- model.matrix(as.formula(~ cov1 + cov2), data@site_cov)
-    expect_equal(result$M, 5)
-    expect_equal(result$code, "112000")
-    expect_equal(result$cov_phi, 1)
-    expect_equal(result$cov_theta, 1)
-    expect_equal(result$cov_psi, mm)
-    expect_equal(result$m_phi, 1)
-    expect_equal(result$m_theta, 2)
-    expect_equal(result$m_psi, 3:5)
-
-    # Interactive effects
-    result <- set_modargs(~ 1, ~ 1, ~ cov1 * cov2, data)
-    mm     <- model.matrix(as.formula(~ cov1 * cov2), data@site_cov)
-    expect_equal(result$M, 6)
-    expect_equal(result$code, "112000")
-    expect_equal(result$cov_phi, 1)
-    expect_equal(result$cov_theta, 1)
-    expect_equal(result$cov_psi, mm)
-    expect_equal(result$m_phi, 1)
-    expect_equal(result$m_theta, 2)
-    expect_equal(result$m_psi, 3:6)
+                       spec_cov = list(cov1 = cov1),
+                       site_cov = list(cov2 = cov2,
+                                       cov3 = cov3))
+    result <- set_modargs(~ 1, ~ 1, ~ 1, NULL, NULL, NULL, data)
+    expect_equal(result$psi, "i")
+    result <- set_modargs(~ 1, ~ 1, ~ cov2, NULL, NULL, NULL, data)
+    expect_equal(result$psi, "ij")
+    expect_error(set_modargs(~ 1, ~ 1, ~ cov1, NULL, NULL, NULL, data),
+                 sprintf("Unexpected terms in formula_psi: %s \n
+                         Only site covariates are allowed for formula_psi.",
+                         "cov1"))
 })
-test_that("Setup for varying-theta/phi model works", {
-    cov1 <- rnorm(2); cov2 <- factor(1:2)
-    data <- occumbData(y = array(0, dim = rep(2, 3)),
-                       site_cov = list(cov1 = cov1,
-                                       cov2 = cov2))
-    expect_error(set_modargs(~ 1, ~ cov1, ~ 1, data),
-                 "Covariates in theta_formula are not yet supported")
-    expect_error(set_modargs(~ cov1, ~ cov1, ~ 1, data),
-                 "Covariates in phi_formula are not yet supported")
-})
+#test_that("Setup for a null model works", {
+#    result <- set_modargs(~ 1, ~ 1, ~ 1,
+#                          occumbData(y = array(0, dim = rep(2, 3))))
+#    expect_equal(result$M, 3)
+#    expect_equal(result$code, "111000")
+#    expect_equal(result$cov_phi, 1)
+#    expect_equal(result$cov_theta, 1)
+#    expect_equal(result$cov_psi, 1)
+#    expect_equal(result$m_phi, 1)
+#    expect_equal(result$m_theta, 2)
+#    expect_equal(result$m_psi, 3)
+#})
+#test_that("Setup for varying-psi model works", {
+#    cov1 <- rnorm(2); cov2 <- factor(1:2)
+#    data <- occumbData(y = array(0, dim = rep(2, 3)),
+#                       site_cov = list(cov1 = cov1,
+#                                       cov2 = cov2))
+#
+#    # Continuous effect
+#    result <- set_modargs(~ 1, ~ 1, ~ cov1, data)
+#    mm     <- model.matrix(as.formula(~ cov1), data@site_cov)
+#    expect_equal(result$M, 4)
+#    expect_equal(result$code, "112000")
+#    expect_equal(result$cov_phi, 1)
+#    expect_equal(result$cov_theta, 1)
+#    expect_equal(result$cov_psi, mm)
+#    expect_equal(result$m_phi, 1)
+#    expect_equal(result$m_theta, 2)
+#    expect_equal(result$m_psi, 3:4)
+#
+#    # Discrete effect
+#    result <- set_modargs(~ 1, ~ 1, ~ cov2, data)
+#    mm     <- model.matrix(as.formula(~ cov2), data@site_cov)
+#    expect_equal(result$M, 4)
+#    expect_equal(result$code, "112000")
+#    expect_equal(result$cov_phi, 1)
+#    expect_equal(result$cov_theta, 1)
+#    expect_equal(result$cov_psi, mm)
+#    expect_equal(result$m_phi, 1)
+#    expect_equal(result$m_theta, 2)
+#    expect_equal(result$m_psi, 3:4)
+#
+#    # Additive effects
+#    result <- set_modargs(~ 1, ~ 1, ~ cov1 + cov2, data)
+#    mm     <- model.matrix(as.formula(~ cov1 + cov2), data@site_cov)
+#    expect_equal(result$M, 5)
+#    expect_equal(result$code, "112000")
+#    expect_equal(result$cov_phi, 1)
+#    expect_equal(result$cov_theta, 1)
+#    expect_equal(result$cov_psi, mm)
+#    expect_equal(result$m_phi, 1)
+#    expect_equal(result$m_theta, 2)
+#    expect_equal(result$m_psi, 3:5)
+#
+#    # Interactive effects
+#    result <- set_modargs(~ 1, ~ 1, ~ cov1 * cov2, data)
+#    mm     <- model.matrix(as.formula(~ cov1 * cov2), data@site_cov)
+#    expect_equal(result$M, 6)
+#    expect_equal(result$code, "112000")
+#    expect_equal(result$cov_phi, 1)
+#    expect_equal(result$cov_theta, 1)
+#    expect_equal(result$cov_psi, mm)
+#    expect_equal(result$m_phi, 1)
+#    expect_equal(result$m_theta, 2)
+#    expect_equal(result$m_psi, 3:6)
+#})
+#test_that("Setup for varying-theta/phi model works", {
+#    cov1 <- rnorm(2); cov2 <- factor(1:2)
+#    data <- occumbData(y = array(0, dim = rep(2, 3)),
+#                       site_cov = list(cov1 = cov1,
+#                                       cov2 = cov2))
+#    expect_error(set_modargs(~ 1, ~ cov1, ~ 1, data),
+#                 "Covariates in theta_formula are not yet supported")
+#    expect_error(set_modargs(~ cov1, ~ cov1, ~ 1, data),
+#                 "Covariates in phi_formula are not yet supported")
+#})
 
 
 ### Set test cases -------------------------------------------------------------
