675bb25048cc79af5755d25857db82d5ee1f061e
Keiichi Fukaya <fukayak99@gmail.com>
2022-04-12 14:04:56 +0900

    Fix tests for cutil

--- R/profile.R
+++ R/profile.R
@@ -1,23 +1,23 @@
-#' @title Conditional utility function for local species diversity assessments.
-#' @param z A species presence-absence matrix (dim = N_species * N_sites).
-#' @param theta A sequence capture probability matrix (dim = N_species * N_sites).
-#' @param phi A relative sequence dominance matrix (dim = N_species * N_sites).
-#' @param K The number of replicates (integer).
-#' @param N Sequence depth (numeric).
-#' @return The expected number of detected species per site.
+# @title Conditional utility function for local species diversity assessments.
+# @param z A species presence-absence matrix (dim = N_species * N_sites).
+# @param theta A sequence capture probability matrix (dim = N_species * N_sites).
+# @param phi A relative sequence dominance matrix (dim = N_species * N_sites).
+# @param K The number of replicates (integer).
+# @param N Sequence depth (numeric).
+# @return The expected number of detected species per site.
 cutil_local <- function(z, theta, phi, K, N) {
     pi <- predict_pi(z, theta, phi, K)
     detect_probs <- predict_detect_probs_local(pi, N)
-    return(sum(detect_probs) / J)
+    return(sum(detect_probs) / ncol(z))
 }
 
-#' @title Conditional utility function for regional species diversity assessments.
-#' @param z A species presence-absence matrix (dim = N_species * N_sites).
-#' @param theta A sequence capture probability matrix (dim = N_species * N_sites).
-#' @param phi A relative sequence dominance matrix (dim = N_species * N_sites).
-#' @param K The number of replicates (integer).
-#' @param N Sequence depth (numeric).
-#' @return The expected total number of species detected over the all sites.
+# @title Conditional utility function for regional species diversity assessments.
+# @param z A species presence-absence matrix (dim = N_species * N_sites).
+# @param theta A sequence capture probability matrix (dim = N_species * N_sites).
+# @param phi A relative sequence dominance matrix (dim = N_species * N_sites).
+# @param K The number of replicates (integer).
+# @param N Sequence depth (numeric).
+# @return The expected total number of species detected over the all sites.
 cutil_regional <- function(z, theta, phi, K, N) {
     pi <- predict_pi(z, theta, phi, K)
     detect_probs <- predict_detect_probs_regional(pi, N)
@@ -29,21 +29,31 @@ predict_pi <- function(z, theta, phi, K) {
     I <- nrow(z)
     J <- ncol(z)
 
-    # Posterior predictive samples of u and r
     u <- r <- array(dim = c(I, J, K))
-    for (k in seq_len(K)) {
-        u[, , k] <- rbinom(I * J, 1, z * theta)
-        r[, , k] <- rgamma(I * J, phi, 1)
+    pi <- array(dim = c(I, J, K))
+
+    count <- 0
+    for (n in seq_len(1000)) {
+        # Posterior predictive samples of u and r
+        for (k in seq_len(K)) {
+            u[, , k] <- stats::rbinom(I * J, 1, z * theta)
+            r[, , k] <- stats::rgamma(I * J, phi, 1)
+        }
+
+        # Derive pi
+        for (j in seq_len(J)) {
+            for (k in seq_len(K)) {
+                ur <- (u * r)[, j, k]
+                pi[, j, k] <- ur / sum(ur)
+            }
+        }
+
+        if (any(is.nan(pi))) count <- count + 1
+        else break
     }
 
-    # Derive pi
-    pi <- array(dim = c(I, J, K))
-    for (j in seq_len(J)) {
-        for (k in seq_len(K)) {
-            ur <- (u * r)[, j, k]
-            pi[, j, k] <- ur / sum(ur)
-        }
-    }
+    if (count == 1000)
+        stop("Failed to generate valid pi values under the given parameter set.")
 
     pi
 }
--- tests/testthat/test-profile.R
+++ tests/testthat/test-profile.R
@@ -1,35 +1,42 @@
 ### Tests for cutil ------------------------------------------------------------
-seed <- rnorm(1)
 I <- 4; J <- 3; K <- 2; N <- 100
-z     <- matrix(sample(c(0, 1), I * J, replace = TRUE), I, J)
-theta <- matrix(runif(I * J), I, J)
-phi   <- matrix(rgamma(I * J, 1), I, J)
-set.seed(seed)
-u <- r <- pi <- array(dim = c(I, J, K))
-for (k in seq_len(K)) {
-    u[, , k] <- rbinom(I * J, 1, z * theta)
-    r[, , k] <- rgamma(I * J, phi, 1)
-}
-for (k in seq_len(K)) {
-    for (j in seq_len(J)) {
-        for (i in seq_len(I)) {
-            pi[i, j, k] <- u[i, j, k] * r[i, j, k] / sum(u[, j, k] * r[, j, k])
+u <- r <- pi <- array(dim = c(I, J, K)); pi[1] <- NaN
+while(any(is.nan(pi))) {
+    z     <- matrix(stats::rbinom(I * J, 1, 0.8), I, J)
+    theta <- matrix(runif(I * J, min = 0.5), I, J)
+    phi   <- matrix(rgamma(I * J, 1), I, J)
+    seed  <- rnorm(1)
+    set.seed(seed)
+
+    for (k in seq_len(K)) {
+        u[, , k] <- rbinom(I * J, 1, z * theta)
+        r[, , k] <- rgamma(I * J, phi, 1)
+    }
+    for (k in seq_len(K)) {
+        for (j in seq_len(J)) {
+            for (i in seq_len(I)) {
+                pi[i, j, k] <- u[i, j, k] * r[i, j, k] / sum(u[, j, k] * r[, j, k])
+            }
         }
     }
 }
 test_that("cutil_local() works as expected", {
     set.seed(seed)
     ans <- sum(predict_detect_probs_local(predict_pi(z, theta, phi, K), N)) / J
+    set.seed(seed)
     expect_equal(cutil_local(z, theta, phi, K, N), ans)
 })
 test_that("cutil_regional() works as expected", {
     set.seed(seed)
     ans <- sum(predict_detect_probs_regional(predict_pi(z, theta, phi, K), N))
+    set.seed(seed)
     expect_equal(cutil_regional(z, theta, phi, K, N), ans)
 })
 test_that("predict_pi() works as expected", {
     set.seed(seed)
     expect_equal(predict_pi(z, theta, phi, K), pi)
+    theta_zero <- matrix(0, I, J)
+    expect_error(predict_pi(z, theta_zero, phi, K), "Failed to generate valid pi values under the given parameter set.")
 })
 test_that("predict_detect_probs_local() works as expected", {
     ans <- matrix(nrow = I, ncol = J)
