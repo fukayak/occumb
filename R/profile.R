26e36e921d9ce14a33ecae753dc3580781a44206
Keiichi Fukaya <fukayak99@gmail.com>
2022-05-27 13:53:51 +0900

    Add document of eval_util_R()

--- R/profile.R
+++ R/profile.R
@@ -105,14 +105,71 @@ eval_util_L <- function(settings,
     out
 }
 
-# @title Tentative: the main function (regional). 
-# @param settings A dataframe of J, K, and N.
-# @param fit An occumbFit object.
-# @param N_rep Controls the sample size for Monte Carlo simulation.
-#   The integral is evaluated using a total of N_sample * N_rep random samples,
-#   where N_sample is the size of the MCMC sample given as `fit.`
-# @param core The number of cores to use for parallelization.
-# @return The expected utility.
+#' @title Expected utility for regional species diversity assessments.
+#'
+#' @description `eval_util_R()` evaluates the expected utility for the regional
+#'  species diversity assessment using Monte Carlo integration.
+#' @details
+#' The utility of the regional species diversity assessment can be defined as
+#'  the number of species in the region of interest expected to be detected
+#'  (Fukaya et al. 2022). `eval_util_R()` evaluates this utility for the region
+#'  modeled in the `occumbFit` object for the combination of `J`, `K`, and `N`
+#'  values specified in the `conditions` argument.
+#'  Such evaluations can be used to balance `J`, `K`, and `N` to maximize the
+#'  utility under a constant budget (possible combinations of `J`, `K`, and `N`
+#'  under a budget level are easily obtained using `x`; see the example below).
+#'  It is also possible to examine how the utility varies with different `J`,
+#'  `K`, and `N` values without setting a budget level, which may be useful to know
+#'  the satisfactory level of `J`, `K`, and `N` from a purely technical point of
+#'  view.
+#' 
+#' The expected utility is defined as the expected value of the conditional
+#'  utility of the form:
+#'  \deqn{U(J, K, N \mid \boldsymbol{r}, \boldsymbol{u}) = \sum_{i = 1}^{I}\left\{1 - \prod_{j = 1}^{J}\prod_{k = 1}^{K}\left(1 - \frac{u_{ijk}r_{ijk}}{\sum_{m = 1}^{I}u_{mjk}r_{mjk}} \right)^N \right\}}{U(J, K, N | r, u) = sum_{i}((1 - \prod_{j}\prod_{k}(1 - (u[i, j, k] * r[i, j, k])/sum(u[, j, k] * r[, j, k])))^N)}
+#'  where \eqn{u_{ijk}}{u[i, j, k]} is a latent indicator variable representing
+#'  the inclusion of the sequence of species \eqn{i}{i} in replicate \eqn{k}{k}
+#'  at site \eqn{j}{j} and \eqn{r_{ijk}}{r[i, j, k]} is a latent variable that
+#'  is proportional to the relative frequency of the sequence of species
+#'  \eqn{i}{i}, conditional on its presence in replicate \eqn{k}{k} at site
+#'  \eqn{j}{j} (Fukaya et al. 2022).
+#' Expectations are taken with respect to the posterior distributions of
+#'  \eqn{\boldsymbol{r} = \{r_{ijk}\}}{r} and \eqn{\boldsymbol{u} = \{u_{ijk}\}}{u},
+#'  which are evaluated numerically by Monte Carlo integration using MCMC
+#'  samples in the `occumbFit` object. Higher approximation accuracy can be
+#'  obtained by increasing the value of `N_rep`.
+#'
+#' The expected utility is evaluated assuming that all sites and replicates are
+#'  homogeneous in the sense that the model parameters are constant across sites
+#'  and replicates. For this reason, in the current version, `eval_util_R()`
+#'  cannot be applied if the supplied `occumbFit` object contains a model with
+#'  site or replicate covariates.
+#'
+#' Monte Carlo integration is executed in parallel on multiple CPU cores where
+#'  the `cores` argument controls the degree of parallelization. By default, all
+#'  cores available in the user's environment are used.
+#' @param settings A data frame that specifies a set of conditions under which
+#'  the utility is evaluated. It must include a column named `J`, `K`, and `N`,
+#'  which specifies the number of sites, the number of replicates per site, and
+#'  the sequencing depth per replicate, respectively.
+#'  `J`, `K`, and `N` must be numeric vectors greater than 0. When `J` and `K`
+#'  contains a decimal, the decimal part is discarded and treated as an integer.
+#'  Additional columns are ignored but may be included.
+#' @param fit An `occumbFit` object containing a posterior sample of the
+#'  relevant parameters.
+#' @param N_rep Controls the sample size for Monte Carlo integration.
+#'   The integral is evaluated using a total of `N_sample * N_rep` random samples,
+#'   where `N_sample` is the size of the MCMC sample provided as `fit`.
+#' @param cores The number of cores to use for parallelization.
+#' @return A data frame with a column named `Utility` in which the estimates of
+#'  expected utility are stored. This is obtained by adding the `Utility` column
+#'  to the data frame provided in the `settings` argument.
+#' @section References:
+#'      K. Fukaya, N. I. Kondo, S. S. Matsuzaki and T. Kadoya (2022)
+#'      Multispecies site occupancy modelling and study design for spatially
+#'      replicated environmental DNA metabarcoding. \emph{Methods in Ecology
+#'      and Evolution} \strong{13}:183--193.
+#'      \url{https://doi.org/10.1111/2041-210X.13732}
+#' @export
 eval_util_R <- function(settings,
                         fit,
                         N_rep = 1,
