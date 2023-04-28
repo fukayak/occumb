f4112b592affac9034971f121ed39e5ad0edfc12
Keiichi Fukaya <fukayak99@gmail.com>
2023-04-28 11:52:32 +0900

    Fix riverbank definition in fish dataset

--- data-raw/fish.R
+++ data-raw/fish.R
@@ -3,6 +3,11 @@ setwd("<occumb directory>")
 devtools::load_all()
 
 load("./data-raw/data_fish.Rdata")
+riverbank_new <- riverbank
+riverbank_new[riverbank_new == 0] <- "with_vegetation"
+riverbank_new[riverbank_new == 1] <- "without_vegetation"
+riverbank <- as.factor(riverbank_new)
+
 fish <- occumbData(
     y = y,
     spec_cov = list(mismatch = mismatch),
