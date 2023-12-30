393cc1dee30650588ed03edc82d3532ff9c81888
Keiichi Fukaya <fukayak99@gmail.com>
2023-12-31 00:30:10 +0900

    Fix wrong tests for `gof()`

--- tests/testthat/test-gof.R
+++ tests/testthat/test-gof.R
@@ -1,18 +1,7 @@
 ### Test data ------------------------------------------------------------------
 I <- 2; J <- 2; K <- 2
 y <- array(sample.int(I * J * K), dim = c(I, J, K))
-spec_cov <- list(cov1 = rnorm(I))
-site_cov <- list(cov2 = rnorm(J), cov3 = factor(1:J))
-repl_cov <- list(cov4 = matrix(rnorm(J * K), J, K))
-data <- occumbData(y = y,
-                   spec_cov = spec_cov,
-                   site_cov = site_cov,
-                   repl_cov = repl_cov)
-data2 <- occumbData(y = array(sample.int((I + 1) * J * K), dim = c(I + 1, J, K)),
-                    spec_cov = list(cov1 = rnorm(I + 1)),
-                    site_cov = site_cov,
-                    repl_cov = repl_cov)
-
+data  <- occumbData(y = y)
 fit <- occumb(data = data,
               n.chains = 1, n.burnin = 10, n.thin = 1, n.iter = 20,
               verbose = FALSE)
@@ -23,12 +12,12 @@ test_that("Dimensions of the output are as expected", {
     expect_identical(out@stats, "Freeman_Tukey")
     expect_equal(length(out@stats_obs), fit@fit$mcmc.info$n.samples)
     expect_equal(length(out@stats_rep), fit@fit$mcmc.info$n.samples)
-    expect_false(any(out@p_value < 0 & 1 < out@p_value))
+    expect_false(any(c(out@p_value < 0, 1 < out@p_value)))
     out <- gof(fit, stats = "deviance", plot = FALSE, cores = 2)
     expect_identical(out@stats, "deviance")
     expect_equal(length(out@stats_obs), fit@fit$mcmc.info$n.samples)
     expect_equal(length(out@stats_rep), fit@fit$mcmc.info$n.samples)
-    expect_false(any(out@p_value < 0 & 1 < out@p_value))
+    expect_false(any(c(out@p_value < 0, 1 < out@p_value)))
 })
 
 ### Tests for quality controls -------------------------------------------------
