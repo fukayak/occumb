ca739ecc9c9e79d4cfbba5c44b61f2ca1334800d
MT-nies <takahashi.mika@nies.go.jp>
2024-10-10 15:25:06 +0900

    [Update] set_design_matrix() error message
    add if() to throw a better error message when the argument is character
    variable and cannot be evaluated

--- R/occumb.R
+++ R/occumb.R
@@ -768,7 +768,11 @@ set_design_matrix <- function(formula, list_cov, omit_intercept = FALSE) {
         out <- tryCatch({
           stats::model.matrix(formula, data = list_cov)
         }, warning = function(w) {
-          stop(conditionMessage(w), "\n")
+          if (typeof(unlist(list_cov)) == "character") {
+            stop("character argument to numeric evaluation\n", call. = FALSE)
+          } else {
+            stop(w, "\n")
+          }
         })
     }
 
