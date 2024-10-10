0a4e92f8a1d3e6a22c443ae4334b1a8b100e59b2
MT-nies <takahashi.mika@nies.go.jp>
2024-10-10 15:55:43 +0900

    [Update] error message

--- R/occumb.R
+++ R/occumb.R
@@ -771,7 +771,7 @@ set_design_matrix <- function(formula, list_cov, omit_intercept = FALSE) {
           if (typeof(unlist(list_cov)) == "character") {
             stop("character argument to numeric evaluation\n", call. = FALSE)
           } else {
-            stop(w, "\n")
+            stop(w, "\n", call. = FALSE)
           }
         })
     }
